213591932c85e39762d15b638c157748dde84cb2
Leo Dinh <ltd.dinh.leo3@gmail.com>
2026-08-27 21:12:44 -0400

    Initial commit

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string first_name = "Leo ";
+    string last_name = "Dinh ";
+    string title = "Aston Gardens Server ";
+    
+    cout << first_name << " " << last_name << endl;
+    cout << title << endl;
+    return 0;
+}
